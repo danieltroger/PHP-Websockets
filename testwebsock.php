328bba272403bc2eaebb298bf85c56e94017f2c0
Daniel <daniel@Daniels-Mini.fritz.box>
2015-05-12 16:24:16 +0200

    better?

--- testwebsock.php
+++ testwebsock.php
@@ -1,14 +1,69 @@
 #!/usr/bin/env php
 <?php
+function shutdown()
+{
+  echo "Shutting down...\r";
+  fclose($GLOBALS['l']);
+  exit("Bye             " . PHP_EOL);
+}
+
+if(function_exists("pcntl_fork"))
+{
+  declare(ticks = 1);
+  define("daemon",true);
+  define("logfile","server.log");
+  echo "Forking to background, ";
+  $fork = pcntl_fork();
+  if ($fork == -1)
+  {
+     die('Could not fork');
+  }
+  else if ($fork)
+  {
+     echo "Parent PID was " . posix_getpid() . ", dying." . PHP_EOL;
+     exit;
+  }
+  else
+  {
+    define("PID",posix_getpid());
+    pcntl_signal(SIGTERM, 'shutdown');
+    pcntl_signal(SIGHUP,  'shutdown');
+    pcntl_signal(SIGABRT, 'shutdown');
+    pcntl_signal(SIGQUIT, 'shutdown');
+    pcntl_signal(SIGILL, 'shutdown');
+  }
+}
+else
+{
+  define("daemon",false);
+}
+if(daemon)
+{
+  echo "We are chind and have PID " . PID . PHP_EOL;
+  $l = fopen(logfile,"w");
+}
 
 require_once('./websockets.php');
 
 class echoServer extends WebSocketServer {
   //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
-
+  public function quit()
+  {
+    foreach($this->users as $u)
+    {
+      $this->send($u,"MSG:SERVER:Disconnecting due to internal shutdown.");
+      $this->disconnect($u->socket);
+    }
+  }
   protected function process ($user, $message)
   {
     $msg = explode(":",$message);
+    if($message == "NAMES")
+    {
+      $nicks = Array();
+      foreach($this->users as $u){if(!$u->hasSentClose){$nicks[] = $u->nick;}}
+      $this->send($user,"MSG:SERVER: Currently connected users are: " . implode(", ",$nicks));
+    }
     if(sizeof($msg) < 2)
     {
       $this->send($user,"BAD_MSG:too_short");
@@ -29,21 +84,27 @@ class echoServer extends WebSocketServer {
         {
           foreach($this->users as $u)
           {
-            if($text == $u->nick)
+            if(!$u->hasSentClose)
             {
-              $this->send($user,"BAD_UNAME:taken");
-              return;
+              if($text == $u->nick)
+              {
+                $this->send($user,"BAD_UNAME:taken");
+                return;
+              }
             }
           }
           if($user->nick == NULL)
           {
-            echo "{$text} connected." . PHP_EOL;
+            $this->stdout("{$text} connected.");
             $user->nick = $text;
             $nicks = Array();
             foreach($this->users as $u)
             {
-              $this->send($u,"JOIN:{$text}");
-              $nicks[] = $u->nick;
+              if(!$u->hasSentClose)
+              {
+                $this->send($u,"JOIN:{$text}");
+                $nicks[] = $u->nick;
+              }
             }
             $this->send($user,"MSG:SERVER:Currently connected users are: " . implode(", ",$nicks));
           }
--- websockets.php
+++ websockets.php
@@ -10,7 +10,6 @@ abstract class WebSocketServer {
   protected $master;
   protected $sockets                              = array();
   protected $users                                = array();
-  protected $interactive                          = true;
   protected $headerOriginRequired                 = false;
   protected $headerSecWebSocketProtocolRequired   = false;
   protected $headerSecWebSocketExtensionsRequired = false;
@@ -27,7 +26,7 @@ abstract class WebSocketServer {
 
   }
 
-  abstract protected function process($user,$message); // Called immediately when the data is recieved. 
+  abstract protected function process($user,$message); // Called immediately when the data is recieved.
   abstract protected function connected($user);        // Called after the handshake response is sent to the client.
   abstract protected function closed($user);           // Called after the connection is closed.
 
@@ -84,32 +83,8 @@ abstract class WebSocketServer {
               $this->doHandshake($user,$buffer);
             }
             else {
-              if (($message = $this->deframe($buffer, $user)) !== FALSE) {
-                if($user->hasSentClose) {
-                  $this->disconnect($user->socket);
-                  $this->stdout("Client disconnected. Sent close: " . $socket);
-                }
-                else {
-                  $this->process($user, $message); // todo: Re-check this.  Should already be UTF-8.
-                }
-              }
-              else {
-                do {
-                  $numByte = @socket_recv($socket,$buffer,$this->maxBufferSize,MSG_PEEK);
-                  if ($numByte > 0) {
-                    $numByte = @socket_recv($socket,$buffer,$this->maxBufferSize,0);
-                    if (($message = $this->deframe($buffer, $user)) !== FALSE) {
-                      if($user->hasSentClose) {
-                        $this->disconnect($user->socket);
-                        $this->stdout("Client disconnected. Sent close: " . $socket);
-                      }
-                      else {
-                       $this->process($user,$message);
-                      }
-                    }
-                  }
-                } while($numByte > 0);
-              }
+              //split packet into frame and send it to deframe
+              $this->split_packet($numBytes,$buffer, $user);
             }
           }
         }
@@ -260,14 +235,24 @@ abstract class WebSocketServer {
   }
 
   public function stdout($message) {
-    if ($this->interactive) {
+    if (!daemon)
+    {
       echo "$message\n";
+    }
+    else
+    {
+      fwrite($GLOBALS['l'],$message . PHP_EOL);
     }
   }
 
   public function stderr($message) {
-    if ($this->interactive) {
+    if (!daemon)
+    {
       echo "$message\n";
+    }
+    else
+    {
+      fwrite($GLOBALS['l'],$message . PHP_EOL);
     }
   }
 
@@ -340,6 +325,60 @@ abstract class WebSocketServer {
     return chr($b1) . chr($b2) . $lengthField . $message;
   }
 
+  //check packet if he have more than one frame and process each frame individually
+  protected function split_packet($length,$packet, $user) {
+    //add PartialPacket and calculate the new $length
+    if ($user->handlingPartialPacket) {
+      $packet = $user->partialBuffer . $packet;
+      $user->handlingPartialPacket = false;
+      $length=strlen($packet);
+    }
+    $fullpacket=$packet;
+    $frame_pos=0;
+    $frame_id=1;
+
+    while($frame_pos<$length) {
+      $headers = $this->extractHeaders($packet);
+      $headers_size = $this->calcoffset($headers);
+      $framesize=$headers['length']+$headers_size;
+      $this->stdout("frame #".$frame_id." position : ".$frame_pos." msglen : ".$headers['length']." + headers_size ".$headers_size." = framesize of ".$framesize);
+
+      //split frame from packet and process it
+      $frame=substr($fullpacket,$frame_pos,$framesize);
+
+      if (($message = $this->deframe($frame, $user,$headers)) !== FALSE) {
+        if ($user->hasSentClose) {
+	  $this->disconnect($user);
+	} else {
+	  if (mb_check_encoding($message,'UTF-8')) {
+	    //$this->stdout("Is UTF-8\n".$message);
+	    $this->process($user, $message);
+	  } else {
+	    $this->stdout("not UTF-8\n");
+	  }
+        }
+      }
+      //get the new position also modify packet data
+      $frame_pos+=$framesize;
+      $packet=substr($fullpacket,$frame_pos);
+      $frame_id++;
+    }
+    $this->stdout("########    PACKET END         #########");
+  }
+
+  protected function calcoffset($headers) {
+    $offset = 2;
+    if ($headers['hasmask']) {
+      $offset += 4;
+    }
+    if ($headers['length'] > 65535) {
+      $offset += 8;
+    } elseif ($headers['length'] > 125) {
+      $offset += 2;
+    }
+    return $offset;
+  }
+
   protected function deframe($message, &$user) {
     //echo $this->strtohex($message);
     $headers = $this->extractHeaders($message);
@@ -364,11 +403,13 @@ abstract class WebSocketServer {
         break;
     }
 
+    /* Deal by split_packet() as now deframe() do only one frame at a time.
     if ($user->handlingPartialPacket) {
       $message = $user->partialBuffer . $message;
       $user->handlingPartialPacket = false;
       return $this->deframe($message, $user);
     }
+    */
 
     if ($this->checkRSVBits($headers,$user)) {
       return false;
